13d6e575ad2cbc79ee395c8ca51c9d877b928d23
Pratham <prathamlashkari841@gmail.com>
2024-09-07 18:18:22 +0530

    setup backend

---
+++ nexcontact/src/main/java/com/nexcontact/NexcontactApplication.java
@@ -0,0 +1,13 @@
+package com.nexcontact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexcontactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexcontactApplication.class, args);
+	}
+
+}
---
+++ nexcontact/src/test/java/com/nexcontact/NexcontactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexcontact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexcontactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
