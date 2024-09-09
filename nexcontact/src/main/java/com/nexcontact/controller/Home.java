8583cff27d5ac0acd267c9f2a5a8b44603756f11
Pratham <prathamlashkari841@gmail.com>
2024-09-09 17:26:34 +0530

    understand themleaf content in html

--- nexcontact/src/main/java/com/nexcontact/controller/Home.java
+++ nexcontact/src/main/java/com/nexcontact/controller/Home.java
@@ -1,13 +1,15 @@
 package com.nexcontact.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Home {
 
   @GetMapping("/homes")
-  public String getHome() {
+  public String getHome(Model md) {
+    md.addAttribute("name", "vaishali");
     return "home";
   }
 }
