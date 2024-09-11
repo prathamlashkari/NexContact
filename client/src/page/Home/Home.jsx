fcd4855f7cdfd521d0d1bbf7451857afe7ac0b49
Pratham <prathamlashkari841@gmail.com>
2024-09-11 17:59:18 +0530

    added theme to home component

--- client/src/page/Home/Home.jsx
+++ client/src/page/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Home.css";
 const Home = ({ theme }) => {
-  return <div className={``}>Home</div>;
+  return <div className={`${theme ? "dark-mode" : ""}`}>Home</div>;
 };
 
 export default Home;
