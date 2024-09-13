3722b75e252c87b8215e872e865c3ceaf0b39df7
Pratham <prathamlashkari841@gmail.com>
2024-09-13 18:16:59 +0530

    added chain

--- nexcontact/src/main/java/com/nexcontact/config/JwtValidator.java
+++ nexcontact/src/main/java/com/nexcontact/config/JwtValidator.java
@@ -14,7 +14,7 @@ public class JwtValidator extends OncePerRequestFilter {
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
-    // TODO Auto-generated method stub
+
     throw new UnsupportedOperationException("Unimplemented method 'doFilterInternal'");
   }
 
