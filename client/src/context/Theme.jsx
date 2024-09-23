e278498d5588c0c49de9b1766806eb44ac2539e4
Pratham <prathamlashkari841@gmail.com>
2024-09-23 16:29:08 +0530

    chaning theme method with using context api

---
+++ client/src/context/Theme.jsx
@@ -0,0 +1,22 @@
+import { createContext, useState, useEffect } from "react";
+
+export const MyTheme = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [dark, setDark] = useState(() => {
+    const savedTheme = localStorage.getItem("myTheme");
+    return savedTheme === "true";
+  });
+
+  const handleChangeTheme = () => {
+    const newTheme = !dark;
+    localStorage.setItem("myTheme", newTheme);
+    setDark(newTheme);
+  };
+
+  return (
+    <MyTheme.Provider value={{ dark, handleChangeTheme }}>
+      {children}
+    </MyTheme.Provider>
+  );
+};
