10efa0f231aa5d919718ac1d8f2d4b202ddf9153
Pratham <prathamlashkari841@gmail.com>
2024-09-30 19:13:08 +0530

    added login mutation

---
+++ client/src/store/Request/AuthRequest.jsx
@@ -0,0 +1,4 @@
+export const loginReq = {
+  email,
+  password,
+};
---
+++ client/src/store/api/AuthApi.jsx
@@ -0,0 +1,17 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const authApi = createApi({
+  reducerPath: "authApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5454" }),
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: "/login",
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation } = authApi;
